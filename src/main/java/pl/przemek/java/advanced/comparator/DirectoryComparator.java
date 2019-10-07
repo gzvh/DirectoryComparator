ade37c09ca8142bc3d94eac27900fe45d2a91947
Przemysław Cieślarek <madflow12@gmail.com>
2019-10-07 18:34:48 +0200

    1.3

--- src/main/java/pl/przemek/java/advanced/comparator/DirectoryComparator.java
+++ src/main/java/pl/przemek/java/advanced/comparator/DirectoryComparator.java
@@ -8,19 +8,30 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class DirectoryComparator {
 
     public static final int MEGA_BYTE = 1024 * 1024;
     public static final int BUFFER_SIZE = 100 * MEGA_BYTE;
+    public static final int TIMEOUT_PER_RESULT_IN_SECONDS = 60;
 
-    public static List<FileDifference> compareDirs(Path dir1, Path dir2, ExecutorService executorService) {
-        var firstDirChecksumF = computeForFilesInDirectory(dir1, executorService);
-        return null;
+    public static List<FileDifference> compareDirs(Path dir1, Path dir2, ExecutorService executorService) throws Exception{
+        var firstDirChecksumsF = computeForFilesInDirectory(dir1, executorService);
+        var secondDirChecksumsF = computeForFilesInDirectory(dir2, executorService);
+        var firstDirChecksums = waitAndCollectToMap(firstDirChecksumsF);
+        var secondDirChecksums = waitAndCollectToMap(secondDirChecksumsF);
+        
+        return findFileDifferences(firstDirChecksums, secondDirChecksums);
+    }
+
+    private static List<FileDifference> findFileDifferences(Map<Path, String> firstDirChecksums, Map<Path, String> secondDirChecksums) {
     }
 
     private static List<Future<Pair<Path, String>>> computeForFilesInDirectory(Path root, ExecutorService executorService) {
@@ -58,5 +69,13 @@ public class DirectoryComparator {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Map<Path, String> waitAndCollectToMap(List<Future<Pair<Path, String>>> futures) throws Exception {
+        var results = new ArrayList<Pair<Path, String>>();
+        for (var future : futures) {
+            results.add(future.get(TIMEOUT_PER_RESULT_IN_SECONDS, TimeUnit.SECONDS));
+        }
+        return results.stream().collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
     }
 }
